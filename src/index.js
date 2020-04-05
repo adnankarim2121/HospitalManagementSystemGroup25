0aaed3654803a13fc204c5e072d4eba3593da59c
Dilpreet <dilpreet.sandhu1@ucalgary.ca>
2020-04-05 17:49:33 -0600

    Fix white page
    Fixes white page when loading and removes routes.js file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Router, Switch, Route} from 'react-router-dom';
+import HomePage from './homePage/HomePage';
 import LoginPage  from './loginPage/LoginPage.js';
 import AdminPage  from './adminPage/AdminPage.js';
 import PatientPage  from './patientPage/PatientPage.js';
 import NursePage  from './nursePage/NursePage.js';
 import DoctorPage  from './doctorPage/DoctorPage.js';
 import './index.css';
+import history from './History';
 
-import Routes from './Routes'; 
 
 ReactDOM.render(
-    <BrowserRouter>
+        <Router history={history}>
+        <Switch>
+            <Route path="/" exact component={HomePage} />
             <Route path='/LoginPage' component={LoginPage}/>
-            <Route path='/AdminPage' component={AdminPage}/>
-            <Route path='/PatientPage' component={PatientPage}/>
-            <Route path='/NursePage' component={NursePage}/>
             <Route path='/DoctorPage' component={DoctorPage}/>
-          
-
-    </BrowserRouter>
+            <Route path='/NursePage' component={NursePage}/>
+            <Route path='/PatientPage' component={PatientPage}/>
+            <Route path='/AdminPage' component={AdminPage}/>    
+        </Switch>
+    </Router>
 
     , document.getElementById('root'));
