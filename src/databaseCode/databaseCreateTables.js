f8fe8a1a134b6c3ebf552ba78a1e991e5243e16c
adnankarim2121 <35471937+adnankarim2121@users.noreply.github.com>
2020-04-10 16:02:59 -0600

    comments to dataBase folder

--- src/databaseCode/databaseCreateTables.js
+++ src/databaseCode/databaseCreateTables.js
@@ -1,8 +1,14 @@
+/*
+Set up nodejs variables to it can access the neccesary APIs such as mysql and express.
+*/
+
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
 
-
+/*
+Create connection with database.
+*/
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -10,11 +16,17 @@ var con = mysql.createConnection({
   database: "HospitalManagementSystem"
 });
 
+/*
+If connection failed, let user know. Else, run query. 
+*/
 con.connect(function(err) {
   if (err) throw err;
 
   var sql = "CREATE TABLE users (email VARCHAR(255), password VARCHAR(255), userType VARCHAR(255))";
 
+  /*
+  If query fails, throw error. Else, show success message. 
+  */
   con.query(sql, function(err, result)
   {
   	if (err) throw err;
--- src/databaseCode/getUser.js
+++ src/databaseCode/getUser.js
@@ -1,8 +1,15 @@
+/*
+Set up nodejs variables to it can access the neccesary APIs such as mysql and express.
+*/
+
 var mysql = require('mysql');
 var express = require('express');
 var router = express.Router();
 
 
+/*
+Create connection with database.
+*/
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -10,8 +17,14 @@ var connection = mysql.createConnection({
   database: "HospitalManagementSystem"
 });
 
+/*
+If connection failed, let user know. Else, run query. 
+*/
 router.get('/', function(req, res, next) {
     res.locals.connection.query('select * from users', function (error, results, fields) {
+    	/*
+  		If query fails, throw error. Else, return results as a JSON object. 
+  		*/
         if(error) throw error;
         res.send(JSON.stringify(results));
     });
