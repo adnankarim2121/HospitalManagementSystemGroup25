ab2ab46566a33da8ef888a0808fb9ee27391de3e
adnankarim2121 <35471937+adnankarim2121@users.noreply.github.com>
2020-04-09 20:42:30 -0600

    comments to files and function name changing

--- src/adminPage/Admin.js
+++ src/adminPage/Admin.js
@@ -83,11 +83,6 @@ class Admin extends React.Component {
     console.log(userInfo);
   }
 
-  /*
-  Display results from database on webpage.
-  */
-  renderProduct = ({ email, password }) => <div key={email}>{password}</div>
-
   render() {
 
     const {userInfo } = this.state;
