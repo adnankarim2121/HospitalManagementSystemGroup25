a556f79ba3f6fd6d83d83ec97837966222f4b2bf
Adnan Karim <adnan.karim@ucalgary.ca>
2024-02-11 16:01:17 -0700

    delete again

--- src/History.js
+++
@@ -1,9 +0,0 @@
-/*
-Importing all necessary components required
-*/
-import { createBrowserHistory as history} from 'history';
-
-/*
-Exporting component so other files can use component.
-*/
-export default history();
